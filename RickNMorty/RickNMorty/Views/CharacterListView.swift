4096c04d6bd2b0ee0545d2efb5b6c5c194dab31a
mesutgdk <mesuttgedik@gmail.com>
2023-07-05 11:50:15 +0300

    feat: spinner for characterVC

--- RickNMorty/RickNMorty/Views/CharacterListView.swift
+++ RickNMorty/RickNMorty/Views/CharacterListView.swift
@@ -10,14 +10,41 @@ import UIKit
 /// View that handles showing list of characters, loader, etc.
 final class CharacterListView: UIView {
 
+    private let viewModel = CharacterListViewViewModel()
+    
+    private let spinner : UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .large)
+        spinner.hidesWhenStopped = true
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        return spinner
+    } ()
+    
+    // MARK: -init
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        translatesAutoresizingMaskIntoConstraints = false
-        backgroundColor = .systemPink
+        
+        setup()
+        layout()
+        
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    private func setup(){
+        translatesAutoresizingMaskIntoConstraints = false
+        spinner.startAnimating()
+    }
+    
+    private func layout(){
+        addSubview(spinner)
+        NSLayoutConstraint.activate([
+            spinner.widthAnchor.constraint(equalToConstant: 100),
+            spinner.heightAnchor.constraint(equalToConstant: 100),
+            spinner.centerXAnchor.constraint(equalTo: centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+    }
 }
