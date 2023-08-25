4142f0c74d44cf4b8a23c21c2a505e2257f0c4d9
mesutgdk <mesuttgedik@gmail.com>
2023-08-25 15:55:04 +0300

    feat: detailedcollection info title label overview

--- RickNMorty/RickNMorty/Views/CharacterDetails/AppCharacterInfoCollectionViewCell.swift
+++ RickNMorty/RickNMorty/Views/CharacterDetails/AppCharacterInfoCollectionViewCell.swift
@@ -13,38 +13,60 @@ final class AppCharacterInfoCollectionViewCell: UICollectionViewCell {
     private  let valueLabel: UILabel = {
        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Earth"
         return label
     }()
     
     private  let titleLabel: UILabel = {
        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Location"
         return label
     }()
     
     private  let iconImageView: UIImageView = {
        let icon = UIImageView()
         icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.image = UIImage(systemName: "globe.americas")
         return icon
+    }()
+    
+    private  let titleContainerView: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .tertiarySystemBackground
+        return view
     }()
     
     
     // MARK: -init
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .tertiarySystemBackground
+        contentView.backgroundColor = .secondarySystemBackground
         contentView.layer.cornerRadius = 7
+        contentView.addSubviews(titleContainerView, titleLabel, iconImageView)
+        titleContainerView.addSubview(titleLabel)
+        setUpConstraints ()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setUpConstraints(){
-        
+    private func setUpConstraints() {
+        NSLayoutConstraint.activate([
+            titleContainerView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            titleContainerView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            titleContainerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            titleContainerView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.33)
+        ])
     }
+    
     override func prepareForReuse() {
         super.prepareForReuse()
+//        valueLabel.text = nil
+//        titleLabel.text = nil
+//        iconImageView.image = nil
     }
     
     public func configure(viewModel: AppCharacterInfoCollectionViewCellViewModel){
