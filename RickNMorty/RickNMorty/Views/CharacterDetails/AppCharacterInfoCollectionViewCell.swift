ed8c17c4c3913a8928c755305a440dc87c73ee2f
mesutgdk <mesuttgedik@gmail.com>
2023-08-25 16:03:03 +0300

    feat: detailedcollection info title label overview contents

--- RickNMorty/RickNMorty/Views/CharacterDetails/AppCharacterInfoCollectionViewCell.swift
+++ RickNMorty/RickNMorty/Views/CharacterDetails/AppCharacterInfoCollectionViewCell.swift
@@ -14,6 +14,8 @@ final class AppCharacterInfoCollectionViewCell: UICollectionViewCell {
        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         label.text = "Earth"
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20, weight: .medium)
         return label
     }()
     
@@ -34,7 +36,7 @@ final class AppCharacterInfoCollectionViewCell: UICollectionViewCell {
     private  let titleContainerView: UIView = {
        let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.backgroundColor = .tertiarySystemBackground
+        view.backgroundColor = .secondarySystemBackground
         return view
     }()
     
@@ -42,7 +44,7 @@ final class AppCharacterInfoCollectionViewCell: UICollectionViewCell {
     // MARK: -init
     override init(frame: CGRect) {
         super.init(frame: frame)
-        contentView.backgroundColor = .secondarySystemBackground
+        contentView.backgroundColor = .tertiarySystemBackground
         contentView.layer.cornerRadius = 7
         contentView.addSubviews(titleContainerView, titleLabel, iconImageView)
         titleContainerView.addSubview(titleLabel)
@@ -54,11 +56,19 @@ final class AppCharacterInfoCollectionViewCell: UICollectionViewCell {
     }
     
     private func setUpConstraints() {
+        // titleContaineView
         NSLayoutConstraint.activate([
             titleContainerView.leftAnchor.constraint(equalTo: contentView.leftAnchor),
             titleContainerView.rightAnchor.constraint(equalTo: contentView.rightAnchor),
             titleContainerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
             titleContainerView.heightAnchor.constraint(equalTo: contentView.heightAnchor, multiplier: 0.33)
+        ])
+            // titleLabel
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: contentView.topAnchor),
+            titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor),
+            titleLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor),
+            titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor)
         ])
     }
     
