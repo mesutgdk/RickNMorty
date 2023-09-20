67ca5a26f4b525fd7b6f0043de661dcd003e347b
mesutgdk <mesuttgedik@gmail.com>
2023-09-20 14:13:39 +0300

    feat: setting viewmodel switch statements

--- RickNMorty/RickNMorty/ViewModels/Settings/AppSettingsOption.swift
+++ RickNMorty/RickNMorty/ViewModels/Settings/AppSettingsOption.swift
@@ -5,4 +5,48 @@
 //  Created by Mesut Gedik on 20.09.2023.
 //
 
-import Foundation
+import UIKit
+
+enum AppSettingsOption: CaseIterable {
+    case rateApp
+    case contactUs
+    case terms
+    case privacy
+    case apiReference
+
+    case viewCode
+    
+    var displayTitle: String {
+        switch self {
+        case .rateApp:
+            return "Rate App"
+        case .contactUs:
+            return "Contact Us"
+        case .terms:
+            return "Terms of Servise"
+        case .privacy:
+            return "Privacy"
+        case .apiReference:
+            return "API Reference"
+        case .viewCode:
+            return "View App Code"
+        }
+    }
+    
+    var icon : UIImage? {
+        switch self {
+        case .rateApp:
+            return UIImage(systemName: "")
+        case .contactUs:
+            return UIImage(systemName: "")
+        case .terms:
+            return UIImage(systemName: "")
+        case .privacy:
+            return UIImage(systemName: "")
+        case .apiReference:
+            return UIImage(systemName: "")
+        case .viewCode:
+            return UIImage(systemName: "")
+        }
+    }
+}
